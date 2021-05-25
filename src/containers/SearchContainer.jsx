f20a9c6ee88d5af8bd99b70cdfdf8b4530047e12
Claudia <claudias.mazariegos@gmail.com>
2021-05-25 15:26:28 -0700

    .

---
+++ src/containers/SearchContainer.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ArtistSearchContainer extends Component {
+  render() {
+    return (
+      <div>
+                
+      </div>
+    );
+  }
+}
