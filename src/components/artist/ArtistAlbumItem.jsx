c75db9b41a56d34b8f27ffc4b7930d65a531d5f3
Nicole Martin <n.martinpdx@gmail.com>
2021-05-25 17:58:30 -0700

    add fetchAlbums func

---
+++ src/components/artist/ArtistAlbumItem.jsx
@@ -0,0 +1,3 @@
+
+// we need to add this to the img src for cover art to render
+<img src=(`http://coverartarchive.org/release/${albumId}/front`);
--- src/services/brainz-api.js
+++ src/services/brainz-api.js
@@ -1,22 +1,28 @@
 export const fetchArtists = async () => {
-
   const res = await fetch(
     `http://musicbrainz.org/ws/2/artist?query=beyonce&fmt=json&limit=25`
   );
 
   const data = await res.json();
-
-//   console.log(data, 'DATA');
-
-
   const artists = data.artists;
 
-//   console.log(artists, 'ARTISTS');
-
-
-  return artists.map(artists => ({
+  return artists.map((artists) => ({
     id: artists.id,
-    name: artists.name
+    name: artists.name,
   }));
+};
 
+export const fetchAlbums = async (artistId) => {
+  const res = await fetch(
+    `http://musicbrainz.org/ws/2/release?artist=${artistId}&fmt=json`
+  );
+
+  const data = await res.json();
+  const albums = data.releases;
+
+  return albums.map((album) => ({
+    albumId: album.id,
+    title: album.title,
+    releaseDate: album.date,
+  }));
 };
