df0f0ad4bbf53ff06af701b695e3614638005073
Nicole Martin <n.martinpdx@gmail.com>
2021-05-27 10:54:39 -0700

    add tests

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,18 +1,18 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 
 export default function Search({ searchQuery, onSearchChange, onSubmit }) {
-
   return (
     <form onSubmit={onSubmit}>
       <label htmlFor="query">Search: </label>
       <input
+        placeholder="Search Artist..."
         id="query"
         type="text"
         value={searchQuery}
         onChange={onSearchChange}
       />
-      <button aria-label="search">Submit</button>        
+      <button aria-label="search">Submit</button>
     </form>
   );
 }
-
